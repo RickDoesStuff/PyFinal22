92e3e4ec0cacf7556722c21d79beffcfeb4ac8dc
AeroEagL <aeroeagl@yahoo.com>
2022-10-12 20:47:44 -0400

    added docs

--- Play.py
+++ Play.py
@@ -4,9 +4,18 @@ class Play:
         pass
 
     def playAll(self):
+        """
+        This function plays all musics in the music list.
+        """
         return
 
     def shuffle(self):
+        """
+        This function calls getQueue() then convert getQueue() into a list, then shuffles the list,
+        then sets the queue into a list
+        :return:
+        """
+
         return
 
     def playSpecific(self):
