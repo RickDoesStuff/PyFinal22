4a019a55dea90ab55ffaa5cabf65e47815c90754
AeroEagL <aeroeagl@yahoo.com>
2022-10-12 20:35:57 -0400

    added function definitions for Play

---
+++ Play.py
@@ -0,0 +1,20 @@
+## Plays
+class Play:
+    def __init__(self):
+        pass
+
+    def playAll(self):
+        return
+
+    def shuffle(self):
+        return
+
+    def play_specific(self):
+        return
+
+    def pause_music(self):
+        return
+
+    def stop_music(self):
+        return
+
