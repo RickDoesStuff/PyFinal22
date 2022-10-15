0fad9916e0d7551c02e891ea4c0bd86bd8b2d1ef
RickDoesStuff <rickyjaquino@gmail.com>
2022-10-15 00:40:16 -0400

    Cleaned up the FileUtils.py fileToSong function

--- Utils/FileUtils.py
+++ Utils/FileUtils.py
@@ -19,15 +19,13 @@ class FileUtils:
 
         with open(fileName, newline="") as file:
             reader = csv.reader(file)
-            print("csv.reader(file): " + str(reader))
-            print("\n rows: \n")
-            notes=[]
+            notes = []
             title = None
             inst = None
             key = None
             sig = None
             for row in reader:
-                if(len(row[0])>2):
+                if len(row) == 4:  # if a row in the file has 4 items, it's the header, and it should define everything
                     title = row[0]
                     inst = row[1]
                     key = row[2]
