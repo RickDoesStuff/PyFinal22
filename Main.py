68a93dc4da72074976af752e04416ec1aa74a614
RickDoesStuff <rickyjaquino@gmail.com>
2022-10-26 14:01:47 -0400

    removed commented code in main.py

--- Main.py
+++ Main.py
@@ -1,32 +1,13 @@
 from Utils import MusicUtils
 from Utils import FileUtils
 
+
 def main():
     songs = []
     fileNames = ["test.csv"]
     load(songs, fileNames)
     MusicUtils.init()
-    #print(str(songs[0]))
-    #songs[0].strPrint()
-    """
-    song = songs[0]
-    print(str(song))
-    song.strPrint()
-    print("======")
-    for i in song.getMusicPerInstList():
-        print(str(i))
 
-    print("\n\n songs")
-    song1 = songs[0]
-    song1.strPrint()
-    print("\n\n musicPer")
-    musicPerInstList = song1.getMusicPerInstList()
-    musicPerInst = musicPerInstList[0]
-    musicPerInst.strPrint()
-    print("\n\n notes")
-    notes = musicPerInst.getNotes()
-    print(notes)
-    """
     print("\n\nPlaying\n\n")
     for musicPerInst in songs[0].getMusicPerInstList():
         for note in musicPerInst.getNotes():
@@ -43,4 +24,3 @@ def load(songs, fileNames):
 
 if __name__ == "__main__":
     main()
-
